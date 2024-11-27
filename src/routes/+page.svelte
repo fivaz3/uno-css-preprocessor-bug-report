af018afbf5c9b4ff21a9f700dfdebe928f540482
fivaz <s.a.fivaz@gmail.com>
2024-11-28 00:43:22 +0100

    a

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<div class="h-28 w-28 shadow">actual</div>
+
+<div class="h-28 w-28 tailwind-shadow">expected</div>
+
+<style>
+    .tailwind-shadow {
+        box-shadow:
+                0 1px 3px 0 rgb(0 0 0 / 0.1),
+                0 1px 2px -1px rgb(0 0 0 / 0.1);
+    }
+</style>
